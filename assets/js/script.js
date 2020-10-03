58d295bb6184dd4fe4cb9c5cfee77ef8fe4f7b63
Yaco-99 <coopman.yannick@gmail.com>
2020-10-03 15:45:54 +0200

    HangMan v1

--- assets/js/script.js
+++ assets/js/script.js
@@ -1,90 +1,22 @@
-//var words = require("an-array-of-french-words");
-const words = [
-  "a",
-  "a-t-elle",
-  "a-t-il",
-  "a-t-on",
-  "abaissa",
-  "abaissable",
-  "abaissables",
-  "abaissai",
-  "abaissaient",
-  "abaissais",
-  "abaissait",
-  "abaissant",
-  "abaissante",
-  "abaissantes",
-  "abaissants",
-  "abaissas",
-  "abaissasse",
-  "abaissassent",
-  "abaissasses",
-  "abaissassiez",
-  "abaissassions",
-  "abaisse",
-  "abaisse-langue",
-  "abaissement",
-  "abaissements",
-  "abaissent",
-  "abaisser",
-  "abaissera",
-  "abaisserai",
-  "abaisseraient",
-  "abaisserais",
-  "abaisserait",
-  "abaisseras",
-  "abaisserez",
-  "abaisseriez",
-  "abaisserions",
-  "abaisserons",
-  "abaisseront",
-  "abaisses",
-  "abaisseur",
-  "abaisseurs",
-  "abaissez",
-  "abaissiez",
-  "abaissions",
-  "abaissons",
-  "abaissâmes",
-  "abaissât",
-  "abaissâtes",
-  "abaissèrent",
-  "abaissé",
-  "abaissée",
-  "abaissées",
-  "abaissés",
-  "abajoue",
-  "abajoues",
-  "abandon",
-  "abandonna",
-  "abandonnai",
-  "abandonnaient",
-  "abandonnais",
-  "abandonnait",
-  "abandonnant",
-  "abandonnas",
-  "abandonnasse",
-  "abandonnassent",
-  "abandonnasses",
-  "abandonnassiez",
-  "abandonnassions",
-  "abandonnataire",
-  "abandonnataires",
-  "abandonne",
-  "abandonnent",
-  "abandonner",
-  "abandonnera",
-  "abandonnerai",
-];
-
 const myFormEvent = document.getElementById("myformEvent");
 let i = 0;
 let count = 0;
 let testIfWin = [];
 let word;
+const draws = [
+  "gallows",
+  "head",
+  "body",
+  "rightHarm",
+  "leftHarm",
+  "rightLeg",
+  "leftLeg",
+  "rightFoot",
+  "leftFoot",
+  "dead",
+];
 
 rand();
-
 const _handleSubmit = async (e) => {
   e.preventDefault();
   const userChoice = document
@@ -112,7 +44,6 @@ function rand() {
     newLetter.innerHTML = `<h2 id="${letterId}" class="border-bottom border-dark">${word[i]}</h2>`;
     document.getElementById("computerWord").appendChild(newLetter);
   }
-  console.log(word);
 }
 
 function check(letter) {
@@ -123,16 +54,20 @@ function check(letter) {
       let gg = document.getElementById(letter + i);
       gg.style.textIndent = "0px";
       testIfWin.push(letter);
-      console.log(testIfWin);
     }
   }
   win == true ? winTest() : lose(count, letter);
 }
 function lose(x, letter) {
+  draw(draws[count]);
   count++;
   let tryLeft = 10 - count;
   if (count == 10) {
-    alert("You lose");
+    alert("You lose, the word was " + word);
+    let youLose = document.createElement("p");
+    youLose.classList.add("text-danger");
+    youLose.innerHTML = `YOU LOSE !`;
+    document.getElementById("winOrLose").appendChild(youLose);
     let input = document.getElementById("playerProposition");
     input.setAttribute("disabled", true);
   } else {
@@ -146,6 +81,10 @@ function lose(x, letter) {
 function winTest() {
   if (testIfWin.length == word.length) {
     alert("You win !");
+    let youWin = document.createElement("p");
+    youWin.classList.add("text-success");
+    youWin.innerHTML = `YOU WIN !`;
+    document.getElementById("winOrLose").appendChild(youWin);
     let input = document.getElementById("playerProposition");
     input.setAttribute("disabled", true);
     count = 0;
@@ -154,6 +93,98 @@ function winTest() {
 }
 function restart() {
   location.reload();
+}
+function draw(part) {
+  const canvas = document.getElementById("hangman");
+  const context = canvas.getContext("2d");
+
+  switch (part) {
+    case "gallows":
+      context.strokeStyle = "#444";
+      context.lineWidth = 10;
+      context.beginPath();
+      context.moveTo(175, 225);
+      context.lineTo(5, 225);
+      context.moveTo(30, 225);
+      context.lineTo(30, 5);
+      context.lineTo(100, 5);
+      context.lineTo(100, 25);
+      context.moveTo(30, 50);
+      context.lineTo(60, 5);
+      context.stroke();
+      break;
+
+    case "head":
+      context.lineWidth = 5;
+      context.beginPath();
+      context.arc(100, 50, 25, 0, Math.PI * 2, true);
+      context.closePath();
+      context.stroke();
+      break;
+
+    case "body":
+      context.beginPath();
+      context.moveTo(100, 75);
+      context.lineTo(100, 140);
+      context.stroke();
+      break;
+
+    case "rightHarm":
+      context.beginPath();
+      context.moveTo(100, 85);
+      context.lineTo(60, 100);
+      context.stroke();
+      break;
+
+    case "leftHarm":
+      context.beginPath();
+      context.moveTo(100, 85);
+      context.lineTo(140, 100);
+      context.stroke();
+      break;
+
+    case "rightLeg":
+      context.beginPath();
+      context.moveTo(100, 140);
+      context.lineTo(80, 190);
+      context.stroke();
+      break;
+
+    case "rightFoot":
+      context.beginPath();
+      context.moveTo(82, 190);
+      context.lineTo(70, 185);
+      context.stroke();
+      break;
+
+    case "leftLeg":
+      context.beginPath();
+      context.moveTo(100, 140);
+      context.lineTo(125, 190);
+      context.stroke();
+      break;
+
+    case "leftFoot":
+      context.beginPath();
+      context.moveTo(122, 190);
+      context.lineTo(135, 185);
+      context.stroke();
+      break;
+
+    case "dead":
+      context.lineWidth = 2;
+      context.beginPath();
+      context.moveTo(85, 37);
+      context.lineTo(95, 45);
+      context.moveTo(95, 37);
+      context.lineTo(85, 45);
+      context.moveTo(104, 37);
+      context.lineTo(114, 45);
+      context.moveTo(114, 37);
+      context.lineTo(104, 45);
+      context.stroke();
+      break;
+  }
 }
 
 myFormEvent.addEventListener("submit", _handleSubmit);
